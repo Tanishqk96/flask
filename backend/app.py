947bb67cc74dbdff43cbd22419de7a19130f0e7d
Tanishk96 <korgaonkartanishq@gmail.com>
2025-02-23 18:55:04 +0530

    cookies and sessions

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,7 @@
-from flask import Flask, request, render_template 
+from flask import Flask, request, render_template , session, make_response
 import pandas as pd
 app = Flask(__name__, template_folder="templates", static_folder='static', static_url_path='/')
-
+app.secret_key = "sessionkey"
 @app.route('/')
 def home():
     return "<h1>Hello, Flask!<h1/>"
@@ -10,6 +10,50 @@ def home():
 def render():
     mylist=[1,2,3,4,5]
     return render_template('index.html', mylist=mylist)
+
+# SETTING SESSIONS AND ALL THE RESPECTIVE OPERATIONS ON IT. 
+#-------------------------------------------------------------
+@app.route('/setsession')
+def set_session():
+    session['username'] = 'tanishq'  # Storing data in session
+    return "Session set!"   
+
+@app.route('/getsession')
+def get_session():
+    username = session.get('username', 'Not set')  # Retrieve session value
+    return f"Stored username: {username}"
+
+@app.route('/deletesession')
+def delete_session():
+    session.pop('username', None)  # Remove 'username' if it exists
+    return "Session deleted!"
+
+@app.route('/clearsession')
+def clear_session():
+    session.clear()  # Clears the entire session
+    return "All session data cleared!"
+
+#-------------------------------------------------------------
+# SETTING COOKIES.
+#-------------------------------------------------------------
+@app.route('/setcookie')
+def set_cookie():
+    response = make_response("Cookie has been set!")
+    response.set_cookie("username", "tanishq", max_age=60*60*24)  # Cookie expires in 1 day
+    return response
+
+@app.route('/getcookie')
+def get_cookie():
+    username = request.cookies.get("username", "No cookie found")  # Retrieve cookie
+    return f"Stored username: {username}"
+
+@app.route('/deletecookie')
+def delete_cookie():
+    response = make_response("Cookie deleted!")
+    response.set_cookie("username", "", expires=0)  # Set expiry to delete cookie
+    return response
+#-------------------------------------------------------------
+
 
 @app.route('/form',methods=['GET','POST'])
 def form():
